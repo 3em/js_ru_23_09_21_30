95440f7ab70e4077a2823b7b040ccd0040b80ba7
Roman <kaboom.ua@gmail.com>
2016-10-11 00:11:08 +0300

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import count from './counter'
 import articles from './articles'
+//лучше объеденить в один filters редюсер
 import filterSelect from './filter-select'
 import filterDate from './filter-date'
 
