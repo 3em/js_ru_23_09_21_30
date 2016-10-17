bf88fe8292f62826f8270719c7c0062de28375f9
Roman <kaboom.ua@gmail.com>
2016-10-17 21:10:54 +0300

    Update comments.js

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -11,7 +11,9 @@ export default (comments = arrayToMap(normalizedComments), action) => {
             const { text, user, newCommentId } = payload
 
             // как можно переопределить лучше newCommentId как id ?
+            // можно { newCommentId: id } = payload,  если очень хочешь
             const id = newCommentId
+            //не мутируй comments
             comments[id] = { id, text, user }
             return comments
         }
