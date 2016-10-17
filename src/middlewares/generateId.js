b6109ac377ea9252821d734ef64496cf1beee405
Roman <kaboom.ua@gmail.com>
2016-10-17 21:07:09 +0300

    Update generateId.js

--- src/middlewares/generateId.js
+++ src/middlewares/generateId.js
@@ -2,7 +2,7 @@ import { ADD_COMMENT } from '../constants'
 import {circleCheckInArray} from '../store/helpers'
 
 export default store => next => action => {
-
+  //Плохо, мидлвары должны быть максимально универсальными, стоит делать так, что б их легко можно было переиспользовать
   if (action.type ==  ADD_COMMENT){
     //проверку по id не успел написать…
     let idComment = Math.floor(Math.random() * (1000 - 10)) + 10;
